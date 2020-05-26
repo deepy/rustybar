08b6add0dc7607d35072d9ac6a885d9ee2737243
Alex Nordlund <alexander.nordlund@cinnober.com>
2020-05-26 14:13:41 +0200

    Add conditionals for platforms.

--- src/bin/warden.rs
+++ src/bin/warden.rs
@@ -1,5 +1,22 @@
-use nix::sys::utsname::uname;
-
+#[cfg(not(target_os = "freebsd"))]
 fn main() {
-    println!("{}", uname().release());
+    println!("Only supported on FreeBSD")
+}
+
+#[cfg(target_os = "freebsd")]
+fn main() {
+    use nix::sys::utsname::uname;
+    use jail::RunningJail;
+
+    let version = uname().release();
+    
+    for jail in RunningJail::all() {
+        let rel = jail.param("osrelease").expect("Could not get osrelease")
+            .unpack_string().expect("osrelease is not a string");
+
+        println!("jail: {} - {}", jail.name().unwrap(), rel);
+        if version != rel {
+            println!(" - OUT OF DATE!");
+        }
+    }
 }
